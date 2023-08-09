1bf63502ae9e8b1e2dd0a307f692a2c315ae792f
JFMajer <jakubmajer@live.com>
2023-08-09 19:15:15 +0200

    question id FromStr

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::io::{Error, ErrorKind};
+use std::str::FromStr;
+
 #[derive(Debug)]
 struct Question {
     id: QuestionId,
@@ -35,9 +38,22 @@ impl std::fmt::Display for QuestionId {
     }
 }
 
+impl FromStr for QuestionId {
+    type Err = std::io::Error;
+
+    fn from_str(id: &str) -> Result<Self, Self::Err> {
+        match id.is_empty() {
+            false => Ok(QuestionId(id.to_string())),
+            true => Err(
+                Error::new(ErrorKind::InvalidInput, "QuestionId cannot be empty")
+            ),
+        }
+    }
+}
+
 fn main() {
     let question = Question::new(
-        QuestionId("1".to_string()),
+        QuestionId::from_str("1").expect("QuestionId cannot be empty"),
         "How to list files in Linux".to_string(),
         "Hello how to list files in linux in current directory".to_string(),
         Some(vec!["linux".to_string(), "bash".to_string()]),
